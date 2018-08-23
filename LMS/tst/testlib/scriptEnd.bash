d3815d56f85dd8e477df7148ecadd52b8faab8a7
ewsdocker <ewsdocker@gmail.com>
2018-08-23 13:16:48 -0600

    Removing aborted branch

--- LMS/tst/testlib/scriptEnd.bash
+++
@@ -1,9 +0,0 @@
-
-[[ ${lmscli_optDebug} -eq 0  &&  ${lmscli_optQueueErrors} -ne 0 ]]  &&  lmsErrorQDispPop
-[[ ${lmscli_optDevelopment} -eq 1 ]] && logName=${dirAppLog} || logName=${dirTestLog}
-
-lmsConioDisplay ""
-lmsConioDisplay "$(tput bold) Log-file: '${lmstst_logName}' $(tput sgr0)"
-
-[[ ${lmscli_optDevelopment} -eq 0 ]] && lmsErrorExitScript "None"  || lmsErrorExitScript "EndOfTest"
-
