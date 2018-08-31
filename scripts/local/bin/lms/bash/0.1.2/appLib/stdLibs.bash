3aa1dddb6caccea445aaf5309fd8c62bba88a296
ewsdocker <ewsdocker@gmail.com>
2018-08-30 20:58:23 -0600

    made standardized source folder layout to match docker scripts folder layout

---
+++ scripts/local/bin/lms/bash/0.1.2/appLib/stdLibs.bash
@@ -0,0 +1,43 @@
+#
+#    	External Scripts
+#
+#         version 0.0.2
+#
+. $lmsbase_dirLib/lmsXCfg.bash
+. $lmsbase_dirLib/lmsCli.bash
+. $lmsbase_dirLib/lmsColorDef.bash
+. $lmsbase_dirLib/lmsConio.bash
+
+. $lmsbase_dirLib/lmsDomC.bash
+. $lmsbase_dirLib/lmsDomD.bash
+. $lmsbase_dirLib/lmsDomN.bash
+. $lmsbase_dirLib/lmsDomR.bash
+
+. $lmsbase_dirLib/lmsDmpVar.bash
+
+. $lmsbase_dirLib/lmsDynSort.bash
+. $lmsbase_dirLib/lmsDynNode.bash
+. $lmsbase_dirLib/lmsDynArray.bash
+
+. $lmsbase_dirLib/lmsError.bash
+. $lmsbase_dirLib/lmsErrorQDisp.bash
+. $lmsbase_dirLib/lmsErrorQ.bash
+
+. $lmsbase_dirLib/lmsHelp.bash
+
+. $lmsbase_dirLib/lmsDeclare.bash
+
+. $lmsbase_dirLib/lmsLog.bash
+. $lmsbase_dirLib/lmsLogRead.bash
+
+. $lmsbase_dirLib/lmsRDomD.bash
+
+. $lmsbase_dirLib/lmsScriptName.bash
+. $lmsbase_dirLib/lmsStack.bash
+. $lmsbase_dirLib/lmsStartup.bash
+. $lmsbase_dirLib/lmsStr.bash
+. $lmsbase_dirLib/lmsUId.bash
+. $lmsbase_dirLib/lmsUtilities.bash
+. $lmsbase_dirLib/lmsXMLParse.bash
+. $lmsbase_dirLib/lmsXPath.bash
+
